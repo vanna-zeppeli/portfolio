94eb91603f93298d5d4dab266346f62a9331546d
vanna-zeppeli <wahyujoestar33@gmail.com>
2026-08-26 12:55:10 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -23,3 +23,18 @@ gsap.to(track, {
         anticipatePin: 1
     }
 });
+
+// Posisi ikon melingkar di belakang foto
+const orbitItems = document.querySelectorAll(".orbit-ring .orbit-item");
+const orbitRadius = 170;
+
+orbitItems.forEach((item, i) => {
+    const angle = (360 / orbitItems.length) * i;
+    const rad = angle * (Math.PI / 180);
+    const x = Math.cos(rad) * orbitRadius;
+    const y = Math.sin(rad) * orbitRadius;
+    item.style.transform = `translate(calc(-50% + ${x}px), calc(-50% + ${y}px))`;
+
+    const face = item.querySelector(".orbit-face");
+    face.style.transform = `rotate(${angle + 90}deg)`;
+});
